b8258441867f81e00c381b8bc4b5356e9ac11e10
Hayrettin DAL <hayretindal@gmail.com>
2023-02-18 10:21:28 +0300

    json-server and httpClientModule added!

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,24 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
-declare let alertify:any;
+import { AlertifyService } from '../services/alertify.service';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent  {
+export class ProductComponent implements OnInit {
+  constructor(private alertifyService:AlertifyService, private http:HttpClient) {
+    
+  }
+  
   title= "Ürün Listesi"
   filterText= ""
-  products : Product[] = [
-    {id:1, name:"Laptop", price: 20000, categoryId:1, description:"Lenova V1", imageUrl:"https://productimages.hepsiburada.net/s/43/375/10756146954290.jpg"},
-    {id:1, name:"Fare", price: 2000, categoryId:2, description:"Gaming Mouse", imageUrl:"https://upload.wikimedia.org/wikipedia/commons/a/ab/Razer_Naga_2014_MMO_Gaming_Mouse_%2814714867599%29.jpg"},
-    {id:1, name:"Laptop", price: 20000, categoryId:1, description:"Lenova V1", imageUrl:"https://productimages.hepsiburada.net/s/43/375/10756146954290.jpg"},
-    {id:1, name:"Laptop", price: 20000, categoryId:1, description:"Lenova V1", imageUrl:"https://productimages.hepsiburada.net/s/43/375/10756146954290.jpg"},
-    {id:1, name:"Laptop", price: 20000, categoryId:1, description:"Lenova V1", imageUrl:"https://productimages.hepsiburada.net/s/43/375/10756146954290.jpg"},
-    {id:1, name:"Laptop", price: 20000, categoryId:1, description:"Lenova V1", imageUrl:"https://productimages.hepsiburada.net/s/43/375/10756146954290.jpg"},
-  ]
-  addToCart(product:string){
-    alertify.success("added!")
+  products : Product[] = []
+  path = "http://localhost:3000/products"
+  addToCart(product:Product){
+    this.alertifyService.success(product.name+ " added!")
   }
+  ngOnInit(): void {
+    this.http.get<Product[]>(this.path).subscribe(data=>{
+     this.products = data
+    })
+   }
 }
